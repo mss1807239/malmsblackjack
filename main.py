9f5c2c6abdd82167b2250dc6b1c9dbdfadce44b2
mss1807239 <160503538+mss1807239@users.noreply.github.com>
2024-02-19 14:13:59 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
 import random 
 
+class Card():
+    def __init__(self):
+
+        # Creates random card upon instatiation
+        
+        card_values ={"Ace": None,
+                      "2": 2,
+                      "3": 3,
+                      "4": 4,
+                      "5": 5,
+                      "6": 6,
+                      "7": 7,
+                      "8": 8,
+                      "9": 9,
+                      "10": 10,
+                      "Jack": 10,
+                      "King": 10,
+                      "Queen": 10}
+        
+        suit_values = ["Diamonds", "Hearts", "Spades", "Clubs"]
+     
+        self.card = random.choice(list(card_values))
+        self.value = card_values[self.card]
+        self.suit = random.choice(suit_values)
+
 
 class player():
     def __init__(self):
